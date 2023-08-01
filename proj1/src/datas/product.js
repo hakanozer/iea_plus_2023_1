b93ac5e93ac02cb7dbf549b6f36bdb3f0e977b3f
Hakan ÖZER <hakanozer02@gmail.com>
2023-08-01 22:22:50 +0300

    Custom Components

---
+++ proj1/src/datas/product.js
@@ -0,0 +1,29 @@
+export const arr =  [
+    {
+      image: "https://picsum.photos/640/360?random=1",
+      title: "TV",
+      detail: "TV Detayı"
+    },
+    {
+      "image": "https://picsum.photos/640/360?random=2",
+      "title": "Kitap",
+      "detail": "Kitap Detayı"
+    },
+    {
+      "image": "https://picsum.photos/640/360?random=3",
+      "title": "Telefon",
+      "detail": "Telefon Detayı"
+    },
+    {
+      "image": "https://picsum.photos/640/360?random=4",
+      "title": "Bilgisayar",
+      "detail": "Bilgisayar Detayı"
+    },
+    {
+      "image": "https://picsum.photos/640/360?random=5",
+      "title": "Oyun",
+      "detail": "Oyun Detayı"
+    }
+  ]
+
+  export const appTitle = 'App Title'
--- proj1/src/pages/Home.js
+++ proj1/src/pages/Home.js
@@ -1,69 +1,38 @@
 import React from 'react'
+import { arr, appTitle } from '../datas/product'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
 
 function Home() {
+
     return (
         <>
-            <h1>App Title</h1>
+            <Navbar/>
+            <h1>{appTitle}</h1>
             <div className="row">
-                <div className='d-none d-md-block mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
+                { arr.map(( item, index ) =>  
+                <div key={index} className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
                     <div className="card">
-                        <img src="https://picsum.photos/640/360?1" className="card-img-top" />
+                        <img src={item.image} className="card-img-top" alt={item.title} title={item.title} />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">{item.title}</h5>
+                            <p className="card-text">{item.detail}</p>
                             <div className='row'>
                                 <div className='mb-1 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6'>
-                                    <a href="#" className="btn btn-danger w-100">Add</a>
+                                    <a href="?#" className="btn btn-danger w-100">Add</a>
                                 </div>
                                 <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6'>
-                                    <a href="#" className="btn btn-primary w-100">Detail</a>
+                                    <a href="?#" className="btn btn-primary w-100">Detail</a>
                                 </div>
                             </div>
                             
                         </div>
                     </div>
                 </div>
-                <div className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
-                    <div className="card">
-                        <img src="https://picsum.photos/640/360?2" className="card-img-top" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
-                    <div className="card">
-                        <img src="https://picsum.photos/640/360?3" className="card-img-top" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
-                    <div className="card">
-                        <img src="https://picsum.photos/640/360?4" className="card-img-top" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
-                    <div className="card">
-                        <img src="https://picsum.photos/640/360?5" className="card-img-top" />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
+                )}
             </div>
+            <Footer/>
         </>
     )
 }
