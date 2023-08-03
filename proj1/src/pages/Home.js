17005605d372effb7bcda1fefa958295f993a017
Hakan ÖZER <hakanozer02@gmail.com>
2023-08-03 20:38:01 +0300

    Menu

--- proj1/src/pages/Home.js
+++ proj1/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { arr, appTitle } from '../datas/product'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Menu from '../components/Menu'
 
 
 function Home() {
@@ -9,28 +10,34 @@ function Home() {
     return (
         <>
             <Navbar/>
-            <h1>{appTitle}</h1>
-            <div className="row">
-                { arr.map(( item, index ) =>  
-                <div key={index} className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3'>
-                    <div className="card">
-                        <img src={item.image} className="card-img-top" alt={item.title} title={item.title} />
-                        <div className="card-body">
-                            <h5 className="card-title">{item.title}</h5>
-                            <p className="card-text">{item.detail}</p>
-                            <div className='row'>
-                                <div className='mb-1 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6'>
-                                    <a href="?#" className="btn btn-danger w-100">Add</a>
+            <div className='row mt-3'>
+                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-3'>
+                    <Menu/>
+                </div>
+                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xl-9 col-xxl-9'>
+                <div className="row">
+                    { arr.map(( item, index ) =>  
+                    <div key={index} className='mb-2 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4'>
+                        <div className="card">
+                            <img src={item.image} className="card-img-top" alt={item.title} title={item.title} />
+                            <div className="card-body">
+                                <h5 className="card-title">{item.title}</h5>
+                                <p className="card-text">{item.detail}</p>
+                                <div className='row'>
+                                    <div className='mb-1 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6'>
+                                        <a href="?#" className="btn btn-danger w-100">Add</a>
+                                    </div>
+                                    <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6'>
+                                        <a href="?#" className="btn btn-primary w-100">Detail</a>
+                                    </div>
                                 </div>
-                                <div className='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6'>
-                                    <a href="?#" className="btn btn-primary w-100">Detail</a>
-                                </div>
+                                
                             </div>
-                            
                         </div>
                     </div>
+                    )}
                 </div>
-                )}
+                </div>
             </div>
             <Footer/>
         </>
