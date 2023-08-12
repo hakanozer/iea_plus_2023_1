03e3d546b635044895bbc9e88cd9ed86d2d2482e
Hakan ÖZER <hakanozer02@gmail.com>
2023-08-12 20:32:48 +0300

    Update Login.tsx

--- proj2/src/pages/Login.tsx
+++ proj2/src/pages/Login.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+ 
+  // fnc Login
+  const fncLogin = (evt: FormEvent) => {
+    evt.preventDefault()
+    console.log(email, password, "fncLogin Call")
+  }
+
   return (
     <>
         <div className='row'>
             <div className='col-xs-12 col-sm-12 col-md-12 col-lg-3 col-xl-4 col-xxl-4'></div>
             <div className='col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-4 col-xxl-4'>
-                
+                <form onSubmit={fncLogin}>
+                  <h2>Admin Login</h2>
+                  <div className='mb-3'>
+                    <input onChange={(evt) => setEmail(evt.target.value)} placeholder='E-Mail' type='email' className='form-control' />
+                  </div>
+                  <div className='mb-3'>
+                    <input onChange={(evt) => setPassword(evt.target.value)} placeholder='Password' type='password' className='form-control' />
+                  </div>
+                  <button className='btn btn-success'>Send</button>
+                </form>
             </div>
             <div className='col-xs-12 col-sm-12 col-md-12 col-lg-3 col-xl-4 col-xxl-4'></div>
         </div>
